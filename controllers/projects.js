3a11fc52e265ad38b4c4edd393e432c922f08a01
Zhanxi Ni <ni.zhanxi@gmail.com>
2019-06-24 14:25:16 -0700

    testing get route/not working for heroku

--- controllers/projects.js
+++ controllers/projects.js
@@ -6,7 +6,7 @@ const Project = require('../models/project');
 
 module.exports = (app) => {
   //INDEX
-  app.get('/cats', (req, res) => {
+  app.get('/', (req, res) => {
       console.log("HELLLLLOOOOOOOO")
       Project.find()
           .then(projects => {
