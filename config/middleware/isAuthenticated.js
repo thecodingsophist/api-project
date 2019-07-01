e46000f27b6d73c7e5b673cc01f220ea7a454bd5
Zhanxi Ni <ni.zhanxi@gmail.com>
2019-07-01 14:10:28 -0700

    [test] is it going into the authentication middleware?

--- config/middleware/isAuthenticated.js
+++ config/middleware/isAuthenticated.js
@@ -1,7 +1,10 @@
 
 //This is middleware for restricting routes a user is not allowed to visit if not logged in
 module.exports = function(req, res, next) {
+    console.log("inside authentication middleware")
     // If the user is logged in, continue with the request to the restricted route
-    if (req.user) { return next(); }
+    if (req.user) {
+         console.log("authentication found user")
+         return next(); }
     // If the user isn't' logged in, redirect them to the login page return
     res.redirect("/"); };
